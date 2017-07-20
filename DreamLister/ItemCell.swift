8c60007ef454b1e129c2fe7a7ea8302b76bbd73f
Sattar Hummatli <settarxan@gmail.com>
2017-07-20 10:19:15 +0400

    Has set IBOutlest to views and configured table view functions

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,20 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Settar Hummetli on 7/20/17.
+//  Copyright © 2017 Settar Hummetli. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var imgThumb: UIImageView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblPrice: UILabel!
+    @IBOutlet weak var lblDetails: UILabel!
+
+    
+
+}
--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+    
 }
 
